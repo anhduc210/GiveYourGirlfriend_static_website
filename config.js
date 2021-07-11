0fe36d6713fa867db35ec0bfe587e17729318b25
anhduc210 <72877434+anhduc210@users.noreply.github.com>
2021-07-11 18:39:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,14 +4,14 @@ const CONFIG = {
     Vì em là bảo Ngọc nên ai cũng muốn có  <3`,
     btnIntro: 'Tiếp ',
     title: 'Nhưng mà thật ra kết phim nó khác ý a nói bừa =)) thôi ',
-    desc: 'Đồng ý sự xin lỗi chân thành này nhé từ tận đáy lòng này nhé ',
+    desc: 'Đồng ý sự xin lỗi chân thành này nhé  ',
     btnYes: 'Có và nhận đc một trái tim ',
     btnNo: 'Không và nhận một ly trà sữa rẻ tiền  ',
     question:'Xin địa chỉ nhà để ship một trái tim tới nèo ',
     btnReply: 'Gửi cho Anh Đức',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'hihi',
-    messDesc: 'Nhận được thì dr ins nha',
+    messDesc: 'Thật ra là vừa rồi em ghi nó ko có lưu lại đc nên có gì dr qua ins ha =)))',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/phamanhduc2001' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.instagram.com/direct/t/anhduc.pp' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
